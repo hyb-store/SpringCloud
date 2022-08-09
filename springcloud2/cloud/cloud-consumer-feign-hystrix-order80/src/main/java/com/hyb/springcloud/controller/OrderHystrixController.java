ed643c2d1c355a6b96e62dc4ba89297c0ea5c379
hyb-store <3063687049@qq.com>
2022-08-09 23:10:48 +0800

    feat:服务降级全局配置

--- springcloud2/cloud/cloud-consumer-feign-hystrix-order80/src/main/java/com/hyb/springcloud/controller/OrderHystrixController.java
+++ springcloud2/cloud/cloud-consumer-feign-hystrix-order80/src/main/java/com/hyb/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,9 @@
 package com.hyb.springcloud.controller;
 
 import com.hyb.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +15,7 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("consumer")
 @Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")// hystrix 全局fallback方法
 public class OrderHystrixController {
 
     @Resource
@@ -24,14 +28,35 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/payment/hystrix/timeout/{id}")
-//    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
-//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
-//    })
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
+    })
     //@HystrixCommand
     public String paymentInfoTimeOut(@PathVariable("id") Integer id) {
         //int age = 10 / 0;
         String result = paymentHystrixService.paymentInfoTimeOut(id);
         return result;
     }
+
+    /**
+     * 超时访问，设置自身调用超时的峰值，峰值内正常运行，超过了峰值需要服务降级 自动调用fallbackMethod 指定的方法
+     * 超时异常或者运行异常 都会进行服务降级
+     *
+     * @param id
+     * @return
+     */
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
+        return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
+    }
+
+    /**
+     * hystrix 全局fallback方法
+     *
+     * @return
+     */
+    public String payment_Global_FallbackMethod() {
+        return "Global异常处理信息，请稍后再试，/(ㄒoㄒ)/~~";
+    }
+
 
 }
--- springcloud2/cloud/cloud-provider-hystrix-payment8001/src/main/java/com/hyb/springcloud/service/PaymentService.java
+++ springcloud2/cloud/cloud-provider-hystrix-payment8001/src/main/java/com/hyb/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.hyb.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -17,6 +19,17 @@ public class PaymentService {
                 + "O(∩_∩)O哈哈~";
     }
 
+
+    /**
+     * 超时访问，设置自身调用超时的峰值，峰值内正常运行，超过了峰值需要服务降级 自动调用fallbackMethod 指定的方法
+     * 超时异常或者运行异常 都会进行服务降级
+     *
+     * @param id
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "paymentInfoTimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
+    })
     public String paymentInfoTimeOut(Integer id) {
         //int age = 10/0;
         int second = 3;
@@ -30,5 +43,17 @@ public class PaymentService {
         System.out.println(end - start);
         return "线程池:  " + Thread.currentThread().getName() + " paymentInfoTimeOut,id:  " + id + "\t"
                 + "O(∩_∩)O哈哈~" + "  耗时(秒): " + second;
+    }
+
+
+    /**
+     * paymentInfoTimeOut 方法失败后 自动调用此方法 实现服务降级 告知调用者 paymentInfoTimeOut 目前无法正常调用
+     *
+     * @param id
+     * @return
+     */
+    public String paymentInfoTimeOutHandler(Integer id) {
+        return "线程池:  " + Thread.currentThread().getName() + "  paymentInfoTimeOutHandler8001系统繁忙或者运行报错，请稍后再试,id:  " + id + "\t"
+                + "o(╥﹏╥)o";
     }
 }
